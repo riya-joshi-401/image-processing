5b4f5a50076033f8138086b4395f5c6a5ef2d8e6
RIYA JOSHI <53979947+riya-joshi-401@users.noreply.github.com>
2020-06-12 12:57:43 +0530

    Add files via upload

--- Assignment 4/Q1.py
+++ Assignment 4/Q1.py
@@ -1,1 +1,19 @@
+''' To find pixel value at (x,y) using MouseCallback '''
 
+import cv2
+
+cap = cv2.VideoCapture (0)
+
+def mouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:  # if left button was clicked ( this is the event that took place) , (x,y) are the coordinates of event
+        print(frame[x,y])
+
+cv2.namedWindow('webcam')
+cv2.setMouseCallback('webcam', mouse)
+
+while True:
+    x, frame = cap.read ()
+    cv2.imshow('webcam', frame)
+
+    if cv2.waitKey (1) & 0xFF == ord('q'):
+        break
