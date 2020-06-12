46bf4e52f5d42da57ae4374414f584f7eca325c0
RIYA JOSHI <53979947+riya-joshi-401@users.noreply.github.com>
2020-06-12 14:07:16 +0530

    Add files via upload

--- Assignment 4/Q3.py
+++ Assignment 4/Q3.py
@@ -1,45 +1,24 @@
-'''Get the warped image using four mouse clicks'''
-
 import cv2
-import numpy as np
+import numpy as np 
 
-img = cv2.imread('n2.jpg')
+img=cv2.imread('n2.jpg')
+pts=[]
+def mouse(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        pts.append((x,y))
 
-clicks = np.zeros(shape = (4, 2), dtype = np.float32)  # storing coordinates of 4 mouse clicks
-index = 0
+    if len(pts)==4:
+        warp(pts)
 
-def mouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        global index
-        global clicks
+def warp(pts):
+    pts1=np.array([pts[0],pts[1],pts[2],pts[3]],np.float32)
+    pts2=np.array([(0,0),(500,0),(0,500),(500,500)],np.float32)
 
-        if (index < 4):
-            clicks[index] = [x, y]
-            index += 1
-
-            if np.all (clicks):   # when all four clicks captured/stored then call wrap function
-                warpImage()
-                print(clicks)
-        else:
-            pass
-
-def warpImage ():
-    global clicks
-
-     #  clicks in order top left , top right , bottom left , bottom right
-
-
-    tl,tr,bl,br = clicks[0] , clicks[1] , clicks[2] , clicks[3]         
+    perspective=cv2.getPerspectiveTransform(pts1,pts2)
+    transformed=cv2.warpPerspective(img,perspective,(500,500))
+    cv2.imshow('Transformed',transformed)
     
-    warpedPoints = np.array ([(0, 0), (500, 0), (0, 500), (500, 500)], dtype = np.float32)
-
-    perspective = cv2.getPerspectiveTransform(clicks, warpedPoints)
-    warped = cv2.warpPerspective(img, perspective, (500, 500))
-
-    cv2.imshow ('warped image', warped)
-
-cv2.namedWindow ('orginal image')
-cv2.setMouseCallback ('orginal image', mouse)
-cv2.imshow ('orginal image', img)
-
+cv2.namedWindow('img')
+cv2.setMouseCallback('img',mouse)
+cv2.imshow('img',img)
 cv2.waitKey(0)
