0768ae53faef165e61ac8453f7b74492f937d184
RIYA JOSHI <53979947+riya-joshi-401@users.noreply.github.com>
2020-06-12 17:09:31 +0530

    Add files via upload

--- Assignment 4/Q1.py
+++ Assignment 4/Q1.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture (0)
 
 def mouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:  # if left button was clicked ( this is the event that took place) , (x,y) are the coordinates of event
-        print(frame[x,y])
+        print(frame[y,x])
 
 cv2.namedWindow('webcam')
 cv2.setMouseCallback('webcam', mouse)
