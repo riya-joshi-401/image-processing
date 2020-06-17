ca8fffb18d95a62d20829d6c1cb9576d751298ec
RIYA JOSHI <53979947+riya-joshi-401@users.noreply.github.com>
2020-06-17 14:36:45 +0530

    Add files via upload

--- Assignment 6/Q1.py
+++ Assignment 6/Q1.py
@@ -1,1 +1,20 @@
+import cv2
+import numpy as np
 
+img = cv2.imread('img.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+kernel = np.ones((13,13))
+dilate = cv2.dilate(gray,kernel)
+
+canny = cv2.Canny(dilate,250,320)
+
+soblely_kernel = np.array([  -1,0,1,
+                              -2,0,2,
+                            -1,0,1 ])
+
+soblely = cv2.filter2D(canny,-1,soblely_kernel)
+
+cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
+cv2.imshow('Frame',soblely)
+cv2.waitKey(0)
